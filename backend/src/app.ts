9d1e22bcf5ffa5daa427942dbb3b156289ea8b51
Ishaq <orakk2211@gmail.com>
2024-04-20 05:27:40 +0500

    add routes and controllrs

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,17 @@
 import express from "express"
 import { config } from "dotenv"
+import morgan from "morgan"
+import appRouter from "./routes/index.js"
 
 const app = express()
 config()
 
 // middleware
 app.use(express.json())
+
+// remove this in production
+app.use(morgan("dev"))
+
+app.use("/api/v1",appRouter)
 
 export default app
---
+++ backend/src/routes/user-routes.ts
@@ -0,0 +1,6 @@
+import { Router } from "express";
+import { getAllUsers } from "../controllers/user-controllers.js";
+
+const userRoutes = Router();
+userRoutes.get("/", getAllUsers)
+export default userRoutes
