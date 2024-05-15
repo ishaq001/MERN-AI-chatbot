7f0781a3c8446be0a6812c5172156a641bd1892e
Ishaq <orakk2211@gmail.com>
2024-05-16 03:03:21 +0500

    logo added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,22 @@
-import React, { useState, ReactElement, JSX } from 'react'
+import React from "react"
 
-import './App.css'
+import "./App.css"
+import Header from "./components/Header"
+import { Route, Routes } from "react-router-dom"
+import { Chat, Home, Login, NotFound, Signup } from "./pages"
 
-function App(){
-  
-  return (
-    <main>
-      Hello world
-    </main>
-  )
+
+function App() {
+	return <main>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/chat" element={<Chat />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </main>
 }
 
 export default App
